3ea1c3d3ebc2bb659a4a5fccc0fffe5d29a121b2
WahyuIndra094 <124643368+WahyuIndra094@users.noreply.github.com>
2023-12-07 10:57:02 +0700

    Update prediksi.py

--- prediksi.py
+++ prediksi.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pickle
 from PIL import Image
+import pandas as pd
+import plotly.express as px
 
-# Load the pre-trained model
 filename = 'modelPrediksi.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 
-# Image
 image = Image.open('Picture.png')
 
-# Credits
+df = pd.read_csv('laptop_data.csv')
+
 with st.container():
-   
+    
     st.title('Prediksi Harga Laptop\n')
     coll1, coll2 = st.columns([3,100])
 
@@ -20,30 +21,36 @@ with st.container():
 
     with coll2:
             st.image(image , width= 550)
+            
+st.dataframe(df)
 
-# Sidebar with Information
+produk_count = df['Company'].value_counts().reset_index()
+produk_count.columns = ['Company', 'Jumlah']
+fig = px.bar(produk_count, x='Company', y='Jumlah', title='Jumlah Produk yang Banyak di Pasaran')
+fig.update_xaxes(title='Company')
+fig.update_yaxes(title='Jumlah')
+st.plotly_chart(fig)
+
+avg_price = df.groupby('Company')['Price'].mean().reset_index()
+fig_avg_price = px.bar(avg_price, x='Company', y='Price', title='Rata-rata Harga Produk per Company')
+fig_avg_price.update_xaxes(title='Company')
+fig_avg_price.update_yaxes(title='Rata-rata Harga')
+st.plotly_chart(fig_avg_price)
 st.sidebar.subheader('Variasi Fitur')
 st.sidebar.markdown('Pilih nilai untuk setiap fitur dan tekan tombol "Prediksi" untuk melihat hasil.')
 
-# Add sliders for features
-ram = st.sidebar.slider('RAM (GB)', 2, 32, 8)
-inches = st.sidebar.slider('Ukuran Layar (Inchi)', 10, 20, 15)
-weight = st.sidebar.slider('Berat (kg)', 1.0, 5.0, 2.0)
-ghz = st.sidebar.slider('GHz', 1.0, 5.0, 2.0)
+ram = st.sidebar.slider('RAM (GB)', 2, 32)
+inches = st.sidebar.slider('Ukuran Layar (Inchi)', 10, 20)
+weight = st.sidebar.slider('Berat (kg)', 1.0, 5.0)
+ghz = st.sidebar.slider('GHz', 1.0, 5.0)
 
-# Button to trigger prediction
 if st.sidebar.button('Prediksi'):
-    # Make prediction using the loaded model
     prediction = round(loaded_model.predict([[ram, inches, weight, ghz]])[0], 2)
     
-    # Display the prediction result
     st.sidebar.success(f'Prediksi harga laptop adalah: Rp.{prediction}')
 
-# Information and Tips Section
 st.subheader('Aplikasi ini membantu Anda memprediksi harga laptop berdasarkan beberapa fitur tertentu.')
 st.subheader('Gunakan slider di sidebar untuk mengatur nilai fitur dan tekan tombol "Prediksi".')
 st.subheader('Pastikan untuk memilih nilai yang sesuai sebelum melakukan prediksi.')
 
-# Footer
-st.markdown('Dibuat dengan ❤️ menggunakan Streamlit oleh kelompok 7')
-
+st.markdown('Dibuat dengan ❤ menggunakan Streamlit oleh kelompok 7')
